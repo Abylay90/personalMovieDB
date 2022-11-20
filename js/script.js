08f77394db0ef7dbbd50c7a56c7b3374aae0aebd
Abylay <abylay90@gmail.com>
2022-11-20 17:15:48 +0700

    functions added

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
-/* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+let numberOfFilms;
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
+function start() {
+    numberOfFilms = +prompt('How many movies you have watched?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies you have watched?', '');
     }
+}
 
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+start();
 
 const personalMovieDB = {
     count : numberOfFilms,
@@ -32,27 +18,49 @@ const personalMovieDB = {
     privat : false
 };
 
-for (let i=0; i<2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),    
-          b = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i=0; i<2; i++) {
+        const a = prompt('One of the latest watched movie?', ''),    
+              b = prompt('Could you rate it from 1 to 10?', '');
+    
+        if (a!=null && b!=null && a!='' && b!='' && a.length<50 && b.length<50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+        } else {
+            console.log('Error');
+            i--;
+        } 
+    }
+}
 
-    if (a!=null && b!=null && a!='' && b!='' && a.length<50 && b.length<50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count<10) {
+        console.log("Not enough movies watched!");
+    } else if (personalMovieDB.count>=10 && personalMovieDB.count<=30){
+        console.log("You are classic viewer!");
+    } else if (personalMovieDB.count>30) {
+        console.log("Movie lover!");
     } else {
-        console.log('Error');
-        i--;
-    } 
+        console.log("Something is wrong!");
+    }
 }
 
-if (personalMovieDB.count<10) {
-    console.log("Not enough movies watched!");
-} else if (personalMovieDB.count>=10 && personalMovieDB.count<=30){
-    console.log("You are classic viewer!");
-} else if (personalMovieDB.count>30) {
-    console.log("Movie lover!");
-} else {
-    console.log("Something is wrong!");
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
 }
 
-console.log(personalMovieDB);
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i=1; i<=3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`Your number "${i}" best favourite genre of movie?`, '');
+    }
+}
+
+writeYourGenres();
